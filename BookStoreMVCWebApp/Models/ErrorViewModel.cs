f743ec72e4e4a865420f9dc3b61ea66f1b431f1b
2030 <2030@OSAMA_HAFEZ_203>
2024-05-23 05:04:50 +0300

    Add project files.

---
+++ BookStoreMVCWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookStoreMVCWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
